359dee551ce65b2a7e4dc6178741376f0db1ea4c
Andrew Karanja <andywanji56@gmail.com>
2023-04-04 14:17:54 +0300

    chore: removed unused code

--- popup.js
+++ popup.js
@@ -37,38 +37,5 @@ function updateUI(isRunning) {
     // Clear the notification dot
     chrome.action.setBadgeText({ text: "" });
   
-    // ...
   });
-  
-  
-// document.addEventListener("DOMContentLoaded", function () {
-//     const intervalInput = document.getElementById("interval");
-//     const startButton = document.getElementById("start");
-//     const stopButton = document.getElementById("stop");
-  
-//     function updateUI(isRunning) {
-//       startButton.disabled = isRunning;
-//       stopButton.disabled = !isRunning;
-//     }
-  
-//     chrome.runtime.sendMessage({ action: "getStatus" }, (response) => {
-//       if (response.isRunning) {
-//         intervalInput.value = response.interval / 1000;
-//         updateUI(true);
-//       } else {
-//         updateUI(false);
-//       }
-//     });
-  
-//     startButton.addEventListener("click", function () {
-//       const interval = parseInt(intervalInput.value) * 1000;
-//       chrome.runtime.sendMessage({ action: "start", interval });
-//       updateUI(true);
-//     });
-  
-//     stopButton.addEventListener("click", function () {
-//       chrome.runtime.sendMessage({ action: "stop" });
-//       updateUI(false);
-//     });
-//   });
   
